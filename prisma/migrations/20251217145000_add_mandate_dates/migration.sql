72da65f8a225aaafacf733481c83e98b8c831657
Benjamin_mug <mugangubenjamin696@gmail.com>
2025-12-17 20:45:37 +0200

    fix: make migration with IF NOT EXISTS

--- prisma/migrations/20251217145000_add_mandate_dates/migration.sql
+++ prisma/migrations/20251217145000_add_mandate_dates/migration.sql
@@ -1,7 +1,7 @@
 -- AlterTable
-ALTER TABLE "profiles" ADD COLUMN     "mandate_end_date" TIMESTAMP(3),
-ADD COLUMN     "mandate_start_date" TIMESTAMP(3);
+ALTER TABLE "profiles" ADD COLUMN IF NOT EXISTS "mandate_end_date" TIMESTAMP(3),
+ADD COLUMN IF NOT EXISTS "mandate_start_date" TIMESTAMP(3);
 
 -- AlterTable
-ALTER TABLE "user_invitations" ADD COLUMN     "mandate_end_date" TIMESTAMP(3),
-ADD COLUMN     "mandate_start_date" TIMESTAMP(3);
+ALTER TABLE "user_invitations" ADD COLUMN IF NOT EXISTS "mandate_end_date" TIMESTAMP(3),
+ADD COLUMN IF NOT EXISTS "mandate_start_date" TIMESTAMP(3);
